c341dc14fabbd6385a5a7cdafa84d6dc4c835eda
An Li <anli@Ans-Mac-mini.local>
2017-04-13 09:43:23 +0800

    add cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
 
   def index
     @products = Product.all
@@ -7,6 +6,17 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
+    else
+      flash[:warning] = "你的购物车内已有此物"
+    end
+    redirect_to :back
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   mount_uploader :image, ImageUploader
+
+  has_many :cart_items
 end
