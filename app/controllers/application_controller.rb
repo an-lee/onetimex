4dd289b0ff1d7ed3a2b8e7274fe504d2608cfbff
An Li <anli@Ans-Mac-mini.local>
2017-04-14 16:10:39 +0800

    add name & avatar column to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def admin_required
     if !current_user.admin?
@@ -13,7 +14,12 @@ class ApplicationController < ActionController::Base
     @current_cart ||= find_cart
   end
 
-  private
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
+  end
 
   def find_cart
     cart = Cart.find_by(id: session[:cart_id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def username(user)
+    if user.name.present?
+      user.name
+    else
+      user.name = user.email.split("@").first
+    end
+  end
+
 end
